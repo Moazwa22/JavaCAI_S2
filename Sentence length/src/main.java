81220d47d0ffbaf2eb9acb57cbe2c93a6c6fc316
Moaz Waleed Monir <moaazwaleed767@gmail.com>
2025-07-06 19:57:30 +0300

    Update main.java

--- Sentence length/src/main.java
+++ Sentence length/src/main.java
@@ -5,7 +5,7 @@ public class main {
       Scanner S = new Scanner(System.in);
       System.out.print("Enter The Sentence : ");
       String Sentence = S.nextLine();
-        System.out.println("The sentence in Uppercase is : " + Sentence.toUpperCase());
+        System.out.println("The sentence in Uppercase : " + Sentence.toUpperCase());
         System.out.println("The number of Characters is : " + Sentence.length());
         System.out.println("The first Character is : " + Sentence.charAt(0));
         }
