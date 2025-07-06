7f7d4c225aae75088a728d0a3e5c15e4324ea757
Moaz Waleed Monir <moaazwaleed767@gmail.com>
2025-07-06 15:35:36 +0300

    Add files via upload

---
+++ main1/src/main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class main {
+    public static void main(String[] args) {
+
+      Scanner S = new Scanner(System.in);
+      System.out.print("Enter The Sentence : ");
+      String Sentence = S.nextLine();
+        System.out.println("The sentence in Uppercase is : " + Sentence.toUpperCase());
+        System.out.println("The number of Characters is : " + Sentence.length());
+        System.out.println("The first Character is : " + Sentence.charAt(0));
+        }
+    }
+
