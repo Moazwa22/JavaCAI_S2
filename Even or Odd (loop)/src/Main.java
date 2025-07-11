b9232981c95795be6f018ed3f5990470b953c756
Moaz Waleed Monir <moaazwaleed767@gmail.com>
2025-07-11 08:33:30 +0300

    Add files via upload

---
+++ Even or Odd (loop)/src/Main.java
@@ -0,0 +1,16 @@
+
+public class Main {
+    public static void main(String[] args) {
+
+        for( int i = 1 ; i<= 10 ; i++){
+            System.out.print( i );
+        if( i % 2 == 0 ){
+            System.out.println(" is Even number");
+
+        }
+        else {
+            System.out.println(" is Odd number");
+        }
+        }
+        }
+    }
